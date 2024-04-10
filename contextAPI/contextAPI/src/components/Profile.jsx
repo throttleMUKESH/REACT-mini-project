68a641fdfdaad398db965b92f4aa13ad8cbbcdd3
Mukesh Chaudhary <chaudharymukesh370@gmail.com>
2024-04-11 05:11:18 +0545

    1st commit

---
+++ contextAPI/contextAPI/src/components/Profile.jsx
@@ -0,0 +1,14 @@
+import React, {useState, useContext}from 'react';
+import UserContext from '../context/UserContext';
+
+function Profile() {
+    const {user} = useContext(UserContext);
+
+    if(!user) return <div>Please Login</div>
+
+  return (
+    <div>Welcome {user.username}</div>
+  )
+}
+
+export default Profile
